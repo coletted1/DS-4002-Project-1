c11dd258bfe147d0b5fc5b14dfdd3b4b8df9fa8f
efredenburgh <157047455+efredenburgh@users.noreply.github.com>
2024-09-29 22:56:17 -0400

    Update 7 - HypothesisTesting.py

--- SCRIPTS/7 - HypothesisTesting.py
+++ SCRIPTS/7 - HypothesisTesting.py
@@ -1,3 +1,17 @@
+'''
+Running this script re-imports the Bechdel movies dataset and cleans the data, making it ready to perform statistical analysis. It puts the 'rating' variable into a binary of
+1 for movies that pass the Bechdel test (rating == 3), and 0 for those that fail (rating != 3). It deletes rows with missing sentiment values, then splits the dataset
+into two groups depending on whether or not the movie passes the Bechdel test.
+
+Then, 2 statistical tests are executed:
+- A two-sample t-test to compare the average sentiment scores of movies that pass or fail the Bechdel test
+- A Mann-Whitney U test (non-parametric) to compare sentiment distributions between the two groups
+Finally, it prints the p-values from both tests to assess statistical significance.
+After the tests are done, the p-values from both tests are printed and ready to analyze for statistical significance, p < 0.05.
+
+This script requires the pandas, mannwhitneyu, and ttest_ind libraries.
+'''
+
 # Re-import the necessary data
 import pandas as pd
 from scipy.stats import ttest_ind, mannwhitneyu
