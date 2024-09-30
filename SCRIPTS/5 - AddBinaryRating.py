ebd1376b74b0f06a2c6ebc193004e30ebd60edf1
efredenburgh <157047455+efredenburgh@users.noreply.github.com>
2024-09-29 22:49:48 -0400

    Update 5 - AddBinaryRating.py

--- SCRIPTS/5 - AddBinaryRating.py
+++ SCRIPTS/5 - AddBinaryRating.py
@@ -1,7 +1,8 @@
 '''
 Add Binary Ratings in Bechdel Dataset
-  Running this script will result in loading a dataset of movies including sentiment scores from IMDb reviews. For further analysis, simplify the 'rating' column into binary classification: 1 for pass (rating == 3), 0 for fail (rating != 3). Additionally, the updated dataset is saved to the same CSV file, 'bechdel_movies.csv'. You can also check the distribution of the new binary 'rating_binary' column
-  dataset to confirm that the proper changes were made.
+  Running this script will result in loading a dataset of movies including sentiment scores from IMDb reviews. For further analysis, simplify the 'rating' column into binary 
+  classification: 1 for pass (rating == 3), 0 for fail (rating != 3). Additionally, the updated dataset is saved to the same CSV file, 'bechdel_movies.csv'. You can also check the 
+  distribution of the new binary 'rating_binary' column dataset to confirm that the proper changes were made.
 
   This script only requires the pandas library.
 '''
